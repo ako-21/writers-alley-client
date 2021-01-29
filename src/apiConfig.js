1ff808e6fca9659b49999647d9bcfd4f774aadae
ako-21 <akeemogle@gmail.com>
2021-01-28 22:12:45 -0500

    changed api link in apiconfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://desolate-journey-24088.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
